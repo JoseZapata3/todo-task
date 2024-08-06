a291e6cda1ed4b885444b614b4afbc695015cda0
JoseZapata3 <josezapata75235@gmail.com>
2024-08-06 09:01:33 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const server = express()
+const port = 3000
+
+server.listen(port,()=>{
+    console.log(`Server listen in port ${port}`)
+})
+
+server.get('/',(req,res)=>{
+    res.send(
+        {
+            "greeting": "Hello World!"
+        }
+    )
+})
